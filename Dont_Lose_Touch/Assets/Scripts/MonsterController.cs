60045ee7942fe9f9a499c04b7b0d84e3cb395dea
Sam <storres6@student.fitchburgstate.edu>
2022-04-03 19:26:17 -0400

    updating the monstercontroller

--- Dont_Lose_Touch/Assets/Scripts/MonsterController.cs
+++ Dont_Lose_Touch/Assets/Scripts/MonsterController.cs
@@ -7,7 +7,6 @@ public class MonsterController : MonoBehaviour
     private Rigidbody rb;
     public GameObject player;
     public float speed = 10;
-    private Vector3 playerPos;
 
     //how long we want the monster to live
     public float lifeSpan = 30;
@@ -23,14 +22,12 @@ public class MonsterController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        playerPos = new Vector3(player.transform.position.x, player.transform.position.y, player.transform.position.z);
-        transform.position = Vector3.MoveTowards(transform.position, playerPos, speed * Time.deltaTime);
+        transform.position = Vector3.MoveTowards(transform.position, player.transform.position, speed * Time.deltaTime);
     }
 
     //destroys monster after however many seconds we assign to lifeSpan
     IEnumerator DestroyMonster()
     {
-        Debug.Log("DEATH TO MONSTER");
         yield return new WaitForSeconds(lifeSpan);
         Destroy(gameObject);
     }
